163319ed84c7edc6f79735b08d9ff85c211fdf84
Mario Salazar <mario10salazar.1995@gmail.com>
2022-11-05 14:01:59 -0500

    Update by Mario Salazar 14:02 11/05/2022

---
+++ myApp/src/app.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  3 10:13:43 2022
+
+@author: Mario
+"""
+
+from flask import Flask
+
+from Config import config
+
+# Routes
+from routes import Conicas
+
+app = Flask(__name__)
+
+
+def page_not_found(error):
+    return "<h1>Not found page</h1>", 404
+
+
+if __name__ == '__main__':
+    app.config.from_object(config['development'])
+
+    # Blueprints
+    app.register_blueprint(Conicas.main, url_prefix='/')
+
+    # Error handlers
+    app.register_error_handler(404, page_not_found)
+    app.run()
